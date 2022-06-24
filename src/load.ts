a3b7a840ce0d0eea76342dc03a22088928383b7d
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-06-24 13:03:51 +0000

    Rename command

--- src/load.ts
+++ src/load.ts
@@ -1,7 +1,7 @@
 import { generate } from "./routes/generate.ts";
 
 async function command(args: string[]) {
-  if (args[0] === "generate") {
+  if (args[0] === "routes:generate") {
     await generate();
   }
 }
