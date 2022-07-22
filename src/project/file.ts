b5ee6fd0dbb22ff5049ddc061c8376278b1542e7
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-07-22 14:20:12 +0000

    feat: Update function to handle file creation

--- src/project/file.ts
+++ src/project/file.ts
@@ -1,12 +1,13 @@
 export async function create(path: string, data: string) {
   try {
-    const fileinfo = await Deno.lstat(path);
-    if (fileinfo.isFile) {
-      throw new Error(`File "${path}" already exists.`);
+    if ((await Deno.lstat(path)).isFile) {
+      throw new Error(`File "${path}" not created! It already exists.`);
     }
   } catch (err) {
     if (err instanceof Deno.errors.NotFound) {
       await Deno.writeTextFile(path, data);
+    } else {
+      console.error(err.message);
     }
   }
 }
