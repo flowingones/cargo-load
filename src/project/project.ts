59d7a24710def309cdbca1224778a8631f9702dc
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-07-22 14:20:29 +0000

    feat: WIP project creation command

--- src/project/project.ts
+++ src/project/project.ts
@@ -1,5 +1,6 @@
 import { InputLoop } from "../deps.ts";
 import { Registry } from "../command.ts";
+import { create as createDir } from "./directory.ts";
 
 import basic from "./types/basic.ts";
 
@@ -10,20 +11,37 @@ registry.add({
   task: basic,
 });
 
-export async function project() {
-  await type();
+export default async () => {
+  const projectName = await ask("Project name:");
+
+  await createDir(projectName);
+  await type(projectName);
+
   return "";
+};
+
+async function ask(question: string): Promise<string> {
+  const input = new InputLoop();
+  const projectName = await input.question(`${question} `, false);
+  if (!projectName) {
+    return ask(question);
+  }
+  return projectName;
 }
 
-async function type() {
+async function type(projectName: string) {
   const input = new InputLoop();
   console.log("What type of application do you want to create?");
   const choice = await input.choose(registry.all().map((cmd) => cmd.names[0]));
+
   const selected = choice.findIndex((bool) => {
     return bool;
   });
-  if (selected < 0) {
+
+  if (typeof selected === "undefined" || selected < 0) {
     console.error("Application type not supported!");
+    return;
   }
-  console.log(registry.all()[selected].task());
+
+  await registry.all()[selected].task(projectName);
 }
