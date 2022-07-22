eec56aa75605b9a1ec65f2402225a3f60393b355
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-07-22 14:20:52 +0000

    feat: WIP Basic application type

--- src/project/types/basic.ts
+++ src/project/types/basic.ts
@@ -1,30 +1,48 @@
 import { create as createFile } from "../file.ts";
+import { create as createDir } from "../directory.ts";
 
 const denoConfigContent = `{
   "importMap": "./import_map.json",
   "tasks": {
     "dev": "deno run --allow-all --watch=./pages,./assets,./src app.ts"
   },
-}`
+}`;
 
 const importMapContent = `{
   "imports": {
     "app/": "./src/",
     "config/": "./config/",
-    "cargo/": "https://deno.land/x/cargo@0.1.41/",
+    "cargo/": "https://deno.land/x/cargo@0.1.41/"
   }
-}`
+}`;
 
-export default async () =>  {
-  await denoConfig();
-  await importMap();
-  return "Basic application created!"
+const appTsContent = `import { bootstrap } from "cargo/mod.ts";
+import { Get } from "cargo/http/mod.ts";
+
+Get("/", () => {
+  return new Response("Hello root!");
+});
+
+(await bootstrap()).run();
+`;
+
+export default async (projectName: string) => {
+  await createDir(`${projectName}/src`);
+  await createDir(`${projectName}/config`);
+  await denoConfig(projectName);
+  await importMap(projectName);
+  await appTs(projectName);
+  return "Basic application created!";
+};
+
+async function appTs(projectName: string) {
+  await createFile(`${projectName}/app.ts`, appTsContent);
 }
 
-async function denoConfig(){
-  await createFile("deno.json",denoConfigContent);
+async function denoConfig(projectName: string) {
+  await createFile(`${projectName}/deno.json`, denoConfigContent);
 }
 
-async function importMap(){
-  await createFile("import_map.json",importMapContent);
+async function importMap(projectName: string) {
+  await createFile(`${projectName}/import_map.json`, importMapContent);
 }
