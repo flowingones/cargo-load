c571aa50ff8d62b8b6e42a5fa3ab25731c2d8c47
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-07-29 14:05:30 +0000

    feat: show commands based on the registry

--- src/load.ts
+++ src/load.ts
@@ -2,28 +2,9 @@ import { Registry } from "./command.ts";
 
 import project from "./project/project.ts";
 import routes from "./routes/routes.ts";
+import upgrade from "./upgrade/upgrade.ts";
 
 const [command, ...args] = Deno.args;
-
-const help = `!---
-! Cargo Load is not production ready.
-! OPTIONS and COMMANDS might change in a future version.
-! Use it with caution!
-!---
-
-Cargo Load is a CLI to manage your Cargo applications
- 
-USAGE:
-  load [OPTIONS] [COMMAND]
-  
-OPTIONS
-  -h, --help
-  -V, --version
-
-COMMANDS
-  project
-  routes
-`;
 
 const CARGO_LOAD_VERSION = `0.0.3`;
 
@@ -31,21 +12,28 @@ const registry = new Registry();
 
 registry.add({
   names: ["-h", "--help"],
-  task: () => {
-    return help;
-  },
+  description: "Show help",
+  task: help,
 });
 registry.add({
   names: ["-V", "--version"],
+  description: 'Show current version of "Cargo Load"',
   task: () => `Cargo Load ${CARGO_LOAD_VERSION}`,
 });
 registry.add({
   names: ["p", "project"],
+  description: "Create a new project structure",
   task: project,
 });
 registry.add({
   names: ["r", "routes"],
+  description: `Generate ".routes.ts" file`,
   task: routes,
+});
+registry.add({
+  names: ["u", "upgrade"],
+  description: `Upgrade "Cargo Load" executable to the lastest version`,
+  task: upgrade,
 });
 
 const task = registry.find(command);
@@ -57,4 +45,41 @@ if (typeof task?.task === "function") {
   console.error(`
 Error: "${command || "no arguments"}" is not a valid command.
 ${help}`);
+}
+
+function help() {
+  return `!---
+! "Cargo Load" is not production ready.
+! OPTIONS and COMMANDS might change in a future version.
+! Use it with caution!
+!---
+
+"Cargo Load" is a CLI to manage your "Cargo" applications
+ 
+USAGE:
+  load [OPTIONS] [COMMAND]
+  
+OPTIONS
+  -h, --help
+  -V, --version
+
+COMMANDS
+${
+    registry.all().map((command) => {
+      return `  ${
+        expand(command.names.join(", "), 20)
+      } <— ${command.description}\n`;
+    }).join("")
+  }`;
+}
+
+function expand(value: string, length: number) {
+  const chars = [...value];
+  const difference = length - chars.length - 1;
+
+  for (let i = 0; i < difference; i++) {
+    chars.push(" ");
+  }
+
+  return chars.join("");
 }
