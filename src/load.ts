f268bae886e703786ec7faf00c6f126eb4ee340e
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-09-01 15:58:54 +0000

    chore: uprade version

--- src/load.ts
+++ src/load.ts
@@ -7,7 +7,7 @@ import upgrade from "./upgrade/upgrade.ts";
 
 const [command, ...args] = Deno.args;
 
-const CARGO_LOAD_VERSION = `0.0.7`;
+const CARGO_LOAD_VERSION = `0.0.8`;
 
 const registry = new Registry();
 
