88977d3e009b55f7475b4850881e70fa9c570d42
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-09-01 15:06:42 +0000

    refactor: cleanup deps

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,1 @@
-export { join, parse } from "https://deno.land/std@0.123.0/path/mod.ts";
-export { parse as parseArgs } from "https://deno.land/std@0.128.0/flags/mod.ts";
+export { join } from "https://deno.land/std@0.153.0/path/mod.ts";
